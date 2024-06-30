d3d2839655af29d2bff6108b7026559f03d4234c
Marsmer <maryan.smerchinskiy@gmail.com>
2024-06-30 14:28:32 +0300

    .

---
+++ mapsTest/src/App.tsx
@@ -0,0 +1,16 @@
+// src/App.tsx
+
+import React from "react";
+import MapComponent from "../../src/MapComponent";
+
+
+
+const App: React.FC = () => {
+ 
+
+  return (
+    <MapComponent/>
+  );
+};
+
+export default App;
